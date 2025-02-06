842e9bb70d5a7f0209986d51d38647338232ff3a
Ather Ali Siddiqui <engr.ather.93@gmail.com>
2025-02-07 00:58:03 +0500

    edit code

--- Connect/ConnectDB.js
+++ Connect/ConnectDB.js
@@ -3,10 +3,10 @@ import "dotenv/config";
 
 // Destructure the environment variables
 const { DB_USER, DB_PASSWORD, DB_NAME } = process.env;
-// const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@smit.9zo1a.mongodb.net/${DB_NAME}&appName=smit`;
+const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@smit.9zo1a.mongodb.net/${DB_NAME}?appName=smit`;
 
 
-const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@smit.9zo1a.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;  // Correct URL format
+// const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@smit.9zo1a.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;  // Correct URL format
 mongoose.connect(url);
 
 export default mongoose;
